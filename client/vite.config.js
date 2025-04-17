12dfb52d410d731bc62e86f6e4ecc14c6cab1b28
PARESH SUTHAR <129194145+pareshsutharr@users.noreply.github.com>
2025-04-17 22:02:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/', // Ensure '/' if you're deploying at root
+    base: './', // very important for correct asset loading!
   plugins: [
     react({
       jsxRuntime: 'automatic'
